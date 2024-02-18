41d847685f29c45a6498aaf4a21ac2ca5f097004
Arthur Baudin <89687104+dd-absol@users.noreply.github.com>
2024-02-18 17:14:21 +0100

    added error handling via http exceptions

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from business_logic import create_groups
 
@@ -11,5 +11,19 @@ class Item(BaseModel):
 
 
 @app.put("/creategroups")
-async def root(item: Item):
-    return {"groups": create_groups(item.names, item.n)}
+async def creategroups(item: Item):
+
+    if item.n <= 0:
+        raise HTTPException(
+            status_code=400,
+            detail="number of groups cannot be negative or null",
+        )
+
+    groups = create_groups(item.names, item.n)
+
+    if groups is None:
+        raise HTTPException(
+            status_code=400, detail="There are less names than groups"
+        )
+
+    return {"groups": groups}
